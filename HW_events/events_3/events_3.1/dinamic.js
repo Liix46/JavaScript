2a83fc118fc3be00eac7ebf0db72dae01f71948a
Бойчев Александр <bojc_tq19@od.itstep.local>
2021-03-11 16:23:31 +0200

    finish events 3/3.1

--- HW_events/events_3/events_3.1/dinamic.js
+++ HW_events/events_3/events_3.1/dinamic.js
@@ -1,1 +1,37 @@
-let item = 
+
+function Input(event){
+    //debugger;
+    let areaElement = event.target;
+    areaElement.firstChild.data = areaElement.value;
+}
+
+function KeyDown(event) {
+
+   // event.preventDefault();
+
+    if (event.ctrlKey && event.code == 'KeyE') {
+        let item = body.innerHTML;
+        item = item.replace(/div/g, "textarea");
+        body.innerHTML = item;
+        
+        let children = body.children; 
+        for (let i = 0; i < children.length; i++) {
+            if(children[i].localName =='textarea'){
+                children[i].addEventListener("input", Input);
+            }
+        }
+    }
+    else if (event.ctrlKey && event.code == 'KeyS') {
+       
+        let item = body.innerHTML;
+        item = item.replace(/textarea/g, "div");
+        body.innerHTML = item;
+    }
+    
+}
+body.addEventListener("keydown", KeyDown);
+
+
+
+
+
