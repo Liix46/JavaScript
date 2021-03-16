c42904f61c052a7192f8f4f64340b14d2938ea09
Бойчев Александр <bojc_tq19@od.itstep.local>
2021-03-16 13:09:43 +0200

    working events 3/3.2

--- HW_events/events_3/events_3.2/dinamic.js
+++ HW_events/events_3/events_3.2/dinamic.js
@@ -1,11 +1,6 @@
 //////////////////////////////////////////////////
-
-function byField(field) {
-    return (a, b) => a[field] > b[field] ? 1 : -1;
-}
-
 function Click(event) {
-
+    //debugger;
     let currentTitle = event.target;
     let elements = document.getElementsByTagName("th");
     let columbIndex = 0;
@@ -18,17 +13,55 @@ function Click(event) {
 
     elements = document.getElementsByTagName('tr');
     let elemColumbs = new Array();
+    let persons = new Map();
 
     for (let i = 1; i < elements.length; i++) {
         elemColumbs.push(elements[i].children[columbIndex]);
     }
+//debugger;
+    for (let i = 1; i < elements.length; i++) {
+        let newPerson = new Person();
+        newPerson.Firstname = elements[i].children[0].innerText;
+        newPerson.Lastname = elements[i].children[1].innerText;
+        newPerson.Age = parseInt(elements[i].children[2].innerText);
+        newPerson.Company = elements[i].children[3].innerText;
+        persons.set(newPerson[currentTitle.innerText], newPerson);
+    }
 
-    elemColumbs.sort();
+    if (currentTitle.innerText == 'Age') {
+        elemColumbs.sort(function (a, b) {
+            return a - b;
+        });
+    }
+    else {
+        elemColumbs.sort(function (a, b) {
+            if (a.innerText > b.innerText) {
+                return 1;
+            }
+            if (a.innerText < b.innerText) {
+                return -1;
+            }
+            // a должно быть равным b
+            return 0;
+        });
+    }
+
+    let keys = new Array();
+    for(let i = 0; i < elemColumbs.length; i++){
+        keys.push(elemColumbs[i].firstChild.data);
+    }
 
     for (let i = 0; i < elemColumbs.length; i++) {
         console.log(elemColumbs[i].innerText);
     }
     debugger;
+
+    for (let i = 1; i < elements[i].children.length; i++) {
+        elements[i].children[0].innerText = persons.get(keys[i-1]).Firstname;
+        elements[i].children[1].innerText = persons.get(keys[i-1]).Lastname;
+        elements[i].children[2].innerText = persons.get(keys[i-1]).Age;
+        elements[i].children[3].innerText = persons.get(keys[i-1]).Company;
+    }
 }
 
 
@@ -36,4 +69,33 @@ let titles = document.getElementsByTagName("th");
 
 for (const title of titles) {
     title.addEventListener('click', Click);
+}
+
+class Person {
+    get Firstname() {
+        return this._firstname;
+    }
+    set Firstname(value) {
+        this._firstname = value;
+    }
+    get Lastname() {
+        return this._lastname;
+    }
+    set Lastname(value) {
+        this._lastname = value;
+    }
+    get Age() {
+        return this._age;
+    }
+    set Age(value) {
+        if (value > 0) {
+            this._age = value;
+        }
+    }
+    get Company() {
+        return this._company;
+    }
+    set Company(value) {
+        this._company = value;
+    }
 }
