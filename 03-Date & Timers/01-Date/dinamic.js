60aa4f6d26c5fc3b3a78fbc6dc979238a79203be
Бойчев Александр <bojc_tq19@od.itstep.local>
2021-03-18 12:22:38 +0200

    work with calendar

--- 03-Date & Timers/01-Date/dinamic.js
+++ 03-Date & Timers/01-Date/dinamic.js
@@ -1,8 +1,24 @@
 ///////////////
 //debugger;
-table.style.backgroundImage = "url('../01-Date/Calendar/1.JPG')";
 
-let Currentdate = new Date(Date.now());
+
+
+// let Currentdate = new Date(Date.now());
+let Currentdate = new Date("2021", "3", "5");
+if(Currentdate.getMonth()+1==1||Currentdate.getMonth()+1 ==2 ||Currentdate.getMonth()+1 ==12){
+    tbody.style.backgroundImage = "url('../01-Date/Calendar/1.JPG')";
+}
+else if(Currentdate.getMonth()+1>2 && Currentdate.getMonth()+1 < 6){
+    tbody.style.backgroundImage = "url('../01-Date/Calendar/2.JPG')";
+}
+else if(Currentdate.getMonth()+1>5 && Currentdate.getMonth()+1 < 9){
+    tbody.style.backgroundImage = "url('../01-Date/Calendar/3.JPG')";
+}
+else{
+    tbody.style.backgroundImage = "url('../01-Date/Calendar/4.JPG')";
+}
+tbody.style.backgroundRepeat = "no-repeat";
+tbody.style.backgroundSize = "cover";
 
 let month = Currentdate.getMonth();
 Currentdate.setDate(1);
@@ -17,9 +33,14 @@ console.log(days);
 console.log(UTCDay);
 
 let Days = document.getElementsByTagName('td');
+
 //debugger;
-for (let i = 0; i < 35; i++) {
+let lenght = 35;
+for (let i = 0; i < lenght; i++) {
     let index = i + UTCDay + 7;
+    if(index == lenght+7)
+        break;   
+        
     if(i< days){
         Days[index].textContent = `${i+1}`;
     }
@@ -27,4 +48,27 @@ for (let i = 0; i < 35; i++) {
         Days[index].textContent = `${i+1-days}`;
     }
 }
-debugger;
+
+Currentdate.setMonth(Currentdate.getMonth()-1);
+let newDate = new Date(Currentdate.getFullYear(), Currentdate.getMonth()-1, Currentdate.getDate());
+
+month = newDate.getMonth();
+days=0;
+while (month == newDate.getMonth()) {
+    newDate.setDate(newDate.getDate() + 1);
+    days++;
+}
+
+
+for(let i = 0; i< UTCDay; i++){
+    let index = i + 7;
+    Days[index].textContent = `${days - UTCDay + i+1}`;
+}
+
+//debugger;
+
+function Click(event){
+    let cell = event.target;
+}
+
+tbody.addEventListener('click',Click);
