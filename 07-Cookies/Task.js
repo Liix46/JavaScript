1b7a83354213fc48862e0d281f48086d20b75bc4
Бойчев Александр <bojc_tq19@od.itstep.local>
2021-04-20 16:58:23 +0300

    work with Cockie

---
+++ 07-Cookies/Task.js
@@ -0,0 +1,26 @@
+class Task {
+    constructor() {
+        this._question = '';
+        this._ansvers = new Array();
+        this._indexRightAnsver = 0;
+    }
+
+    get question() {
+        return this._question;
+    }
+    get ansvers() {
+        return this._ansvers;
+    }
+    get indexRightAnsver(){
+        return this._indexRightAnsver;
+    }
+    set question(value) {
+        this._question = value;
+    }
+    set ansvers(value){
+        this._ansvers = value;
+    }
+    set indexRightAnsver(value){
+        this._indexRightAnsver = value;
+    }
+}
---
+++ 07-Cookies/index.js
@@ -0,0 +1,59 @@
+window.addEventListener('load', main);
+let tasks = new Array();
+let indexQuestion =1;
+let userRightAnsver =0;
+let RadioBtns = document.getElementsByName('RDBTN');
+function main(){
+   
+    let task = new Task();
+    task._question = " let str = \"Hello\";\n str.something = 5;\n alert(str.something);";
+    task._ansvers = ['5','undefined', 'Будет ошибка.'];
+    task._indexRightAnsver = 2;
+    
+    let task2 = new Task();
+    task2._question = " let arr = [1, 2, 3];\n arr.something = 5;\n alert(arr.something);";
+    task2._ansvers = ['5','undefined', 'Будет ошибка.'];
+    task2._indexRightAnsver = 0;
+
+    let task3 = new Task();
+    task3._question = " Что такое ECMAScript?";
+    task3._ansvers = ['Новый язык программирования.', 'Переработанная реализация Javascript.', 'Спецификация языка Javascript.'];
+    task3._indexRightAnsver = 2;
+
+    tasks.push(task);
+    tasks.push(task2);
+    tasks.push(task3);
+
+    buttonStartTest.addEventListener('click', testing);
+    nextQuestion.addEventListener('click', next);
+}
+
+function testing() {
+    //debugger;
+    question.innerText = tasks[0].question;
+    ansver1.innerText = tasks[0]._ansvers[0];
+    ansver2.innerText = tasks[0]._ansvers[1];
+    ansver3.innerText = tasks[0]._ansvers[2];
+}
+
+function next(){
+    //debugger;
+    let index = tasks[indexQuestion-1]._indexRightAnsver;
+        if(RadioBtns[index].checked ==true){
+            userRightAnsver++;
+        }
+
+    if (indexQuestion == tasks.length) {
+        //alert(userRightAnsver);
+        let name = prompt('Страна требует знать имя своего героя?', 'Имя');
+        alert(`Поздравляем, ${name}! Тест сдан на ${userRightAnsver} из ${tasks.length}`);
+    }
+    else{
+        question.innerText = tasks[indexQuestion].question;
+        ansver1.innerText = tasks[indexQuestion]._ansvers[0];
+        ansver2.innerText = tasks[indexQuestion]._ansvers[1];
+        ansver3.innerText = tasks[indexQuestion]._ansvers[2];
+        indexQuestion++;       
+    }
+   
+}
